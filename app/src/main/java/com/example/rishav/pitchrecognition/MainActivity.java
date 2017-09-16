1572c7e98551b2203dc8acacd7786d859dbe952d
Tanuj Nayak <tanujnay112@live.com>
2017-09-16 14:55:23 -0400

    Added openCV module.

--- app/src/main/java/com/example/rishav/pitchrecognition/MainActivity.java
+++ app/src/main/java/com/example/rishav/pitchrecognition/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.rishav.pitchrecognition;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.opencv.android.OpenCVLoader;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
