6361aa8bfee1021e77a5d38fb3014b344e6069cb
Tanuj Nayak <tanujnay112@live.com>
2017-09-16 23:47:04 -0400

    Can record

--- app/src/main/java/com/example/rishav/pitchrecognition/MainActivity.java
+++ app/src/main/java/com/example/rishav/pitchrecognition/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.rishav.pitchrecognition;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED || (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_DENIED)){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, 100);
+        }
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2VideoFragment.newInstance())
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/rishav/pitchrecognition/Process.java
@@ -0,0 +1,16 @@
+package com.example.rishav.pitchrecognition;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+/**
+ * Created by Tanuj on 9/16/17.
+ */
+
+public class Process {
+
+}
